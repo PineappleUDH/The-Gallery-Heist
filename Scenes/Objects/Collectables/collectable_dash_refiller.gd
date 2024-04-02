b370f5b6fe374bba7977078755f14295c3a87a00
Val <147787102+Valsprites@users.noreply.github.com>
2024-04-01 22:13:08 -0400

    Spruced up dash re-charge sprite

--- Scenes/Objects/Collectables/collectable_dash_refiller.gd
+++ Scenes/Objects/Collectables/collectable_dash_refiller.gd
@@ -2,6 +2,7 @@ extends "res://Scenes/Objects/Collectables/collectable.gd"
 
 @onready var _collider : CollisionShape2D = $CollisionShape2D
 @onready var _reset_timer = $ResetTimer
+@onready var _sprite = $Sprite2D
 
 
 func _ready():
@@ -12,9 +13,10 @@ func _collected(player : Player):
 		player.refill_dash()
 		_collider.set_deferred("disabled", true)
 		modulate.a = 0.2
-		
+		_sprite.frame = 1
 		_reset_timer.start()
 
 func _on_reset_timer_timeout():
 	_collider.disabled = false
+	_sprite.frame = 0
 	modulate.a = 1.0
