02431b328e5a3007430c19d75a6eb7685bd8ad2c
Val <147787102+Valsprites@users.noreply.github.com>
2024-04-04 21:45:19 -0400

    some more work on the dash trail

--- Scenes/Objects/Characters/Saul/dashtrail2.gd
+++ Scenes/Objects/Characters/Saul/dashtrail2.gd
@@ -3,9 +3,14 @@ extends Sprite2D
 var _direction
 var _shrink_factor : float = 0.05
 
-
+func _ready():
+	_direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
+	print(_direction)
 func _process(delta):
-	scale.y -= 0.05
+	if _direction.x > 0 or _direction.x < 0:
+		scale.y -= 0.05
+	if _direction.y > 0 or _direction.y < 0:
+		scale.x -= 0.05
 	
-	if scale.y <= 0:
+	if scale.y <= 0 or scale.x <= 0:
 		queue_free()
