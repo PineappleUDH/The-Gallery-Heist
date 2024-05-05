d2670817de7bc97e01f441fb67f9ff1a4efcafab
Val <147787102+Valsprites@users.noreply.github.com>
2024-05-05 14:58:55 -0400

    added false wall to tilemap

--- Scenes/Objects/Characters/NPCs/npc_click.gd
+++ Scenes/Objects/Characters/NPCs/npc_click.gd
@@ -1,9 +1,10 @@
-extends "res://Scenes/Objects/Characters/NPC/npc.gd"
+extends "res://Scenes/Objects/Characters/NPCs/npc.gd"
 
 
 var _first_meeting : bool = true
 
 func _on_interaction_area_body_entered(body : Node2D):
-	if _first_meeting == true:
-		_first_meeting = false
-		World.level.dialogue_player.play_dialogue()
+	pass
+#	if _first_meeting == true:
+#		_first_meeting = false
+#		World.level.dialogue_player.play_dialogue()
