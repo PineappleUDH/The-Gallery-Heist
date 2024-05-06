f218e7adc61ae1fb1fa035721803d542da757c91
Val <147787102+Valsprites@users.noreply.github.com>
2024-05-06 03:18:35 -0400

    update to labyrinth_val, updates to Click, Updates to instancedock, added Dialogue frame

--- Scenes/Objects/Characters/NPCs/npc_click.gd
+++ Scenes/Objects/Characters/NPCs/npc_click.gd
@@ -2,9 +2,40 @@ extends "res://Scenes/Objects/Characters/NPCs/npc.gd"
 
 
 var _first_meeting : bool = true
+var _opening_lines : Array[Dialogue] = [
+	Dialogue.create(
+		"Oh, hey, you're that Saul guy? You're even balder than I thought you'd be",
+		"Click"
+	),
+	Dialogue.create(
+		"And now I'm being heckled by a talking mouse, lovely",
+		"Saul"
+	),
+	Dialogue.create(
+		"Just having some fun with you, Saul, good luck in the trial!",
+		"Click"
+	),
+	Dialogue.create(
+		"CONSTRUCTION STATUS: 
+			X = UNDER CONSTRUCTION
+			O = COMPLETE
+		GREEN [X]
+		BLUE [X]
+		RED [O]"
+	)
+]
+
+@onready var _trigger_dialogue = $TriggerDialogue
+
+func _process(delta):
+	pass
 
 func _on_interaction_area_body_entered(body : Node2D):
-	pass
-#	if _first_meeting == true:
-#		_first_meeting = false
-#		World.level.dialogue_player.play_dialogue()
+	if _first_meeting == true:
+		World.level.dialogue_player.play_dialogue(_opening_lines, true)
+
+
+func _on_interaction_area_body_exited(body):
+	if body is Player and _first_meeting == true:
+		_trigger_dialogue.monitoring = true
+		_first_meeting = false
