1cd47382deeaefc28c1046ec938ed7066cda0459
Val <147787102+Valsprites@users.noreply.github.com>
2024-05-07 02:23:48 -0400

    updates to labyrinth_val

--- Scenes/Objects/Characters/NPCs/npc_click.gd
+++ Scenes/Objects/Characters/NPCs/npc_click.gd
@@ -2,26 +2,43 @@ extends "res://Scenes/Objects/Characters/NPCs/npc.gd"
 
 
 var _first_meeting : bool = true
+var _disable : bool = false
 var _opening_lines : Array[Dialogue] = [
 	Dialogue.create(
-		"Oh, hey, you're that Saul guy? You're even balder than I thought you'd be",
+		"Oh, hey, you're that [color=#1ebc73]Saul[/color] guy? 
+		
+		You're even balder than I thought you'd be",
 		"Click"
 	),
 	Dialogue.create(
-		"And now I'm being heckled by a talking mouse, lovely",
+		"Oh wow, a talking mouse?",
 		"Saul"
 	),
 	Dialogue.create(
-		"Just having some fun with you, Saul, good luck in the trial!",
+		"Yeah, the names [color=#4d9be6]Click[/color], we're gonna be in this game together!",
 		"Click"
 	),
 	Dialogue.create(
-		"CONSTRUCTION STATUS: 
-			X = UNDER CONSTRUCTION
-			O = COMPLETE
-		GREEN [X]
-		BLUE [X]
-		RED [O]"
+		"So you're [color=#4d9be6]Click, The Mouse[/color]? That's cute",
+		"Saul"
+	),
+	Dialogue.create(
+		"[shake]uhh yeah..[/shake] you can just call me [color=#4d9be6]Click[/color]",
+		"Click"
+	),
+	Dialogue.create(
+		"[i]Anyway,[/i] good luck in there!",
+		"Click"
+	),
+	Dialogue.create(
+		"[color=#f9c22b]CONSTRUCTION STATUS:[/color] 
+		
+		[color=#e83b3b]X=UNDER CONSTRUCTION[/color]
+		[color=#239063]O=COMPLETE[/color]
+		
+		[color=#239063]GREEN PATH[/color] [color=#e83b3b][X][/color]
+		[color=#4d9be6]BLUE PATH[/color] [color=#e83b3b][X][/color]
+		[color=#e83b3b]RED PATH[/color] [color=#239063][O][/color]"
 	)
 ]
 
@@ -31,11 +48,15 @@ func _process(delta):
 	pass
 
 func _on_interaction_area_body_entered(body : Node2D):
-	if _first_meeting == true:
-		World.level.dialogue_player.play_dialogue(_opening_lines, true)
+	if _disable == false:
+		match _first_meeting:
+			true:
+				World.level.dialogue_player.play_dialogue(_opening_lines, true)
+			false:
+				_trigger_dialogue.monitoring = true
+				_disable = true
 
 
 func _on_interaction_area_body_exited(body):
 	if body is Player and _first_meeting == true:
-		_trigger_dialogue.monitoring = true
 		_first_meeting = false
